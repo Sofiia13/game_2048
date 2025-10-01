db841f43f4214cf031156e2cfc029ea2eca11610
Sofiia13 <sophiyastanish@gmail.com>
2025-10-01 20:30:54 +0300

    create 2048 game

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: "@mate-academy/eslint-config",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
-'use strict';
+"use strict";
+import "../styles/main.scss";
+
+import Game from "../modules/Game.class.js";
+const game = new Game();
+
+const mainButton = document.querySelector(".button");
+
+mainButton.addEventListener("click", () => {
+  if (mainButton.classList.contains("start")) {
+    game.start();
+  } else if (mainButton.classList.contains("restart")) {
+    game.restart();
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowLeft":
+      game.moveLeft();
+      game.getScore();
+      break;
+    case "ArrowRight":
+      game.moveRight();
+      game.getScore();
+      break;
+    case "ArrowUp":
+      game.moveUp();
+      game.getScore();
+      break;
+    case "ArrowDown":
+      game.moveDown();
+      game.getScore();
+      break;
+  }
+});
