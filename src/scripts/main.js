d683702d9f46ec49798ad8fa31f6ff75ba784c42
Sofiia13 <sophiyastanish@gmail.com>
2025-10-01 20:31:05 +0300

    small changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,35 @@
-"use strict";
-import "../styles/main.scss";
 
-import Game from "../modules/Game.class.js";
+import '../styles/main.scss';
+
+import Game from '../modules/Game.class.js';
+
 const game = new Game();
 
-const mainButton = document.querySelector(".button");
+const mainButton = document.querySelector('.button');
 
-mainButton.addEventListener("click", () => {
-  if (mainButton.classList.contains("start")) {
+mainButton.addEventListener('click', () => {
+  if (mainButton.classList.contains('start')) {
     game.start();
-  } else if (mainButton.classList.contains("restart")) {
+  } else if (mainButton.classList.contains('restart')) {
     game.restart();
   }
 });
 
-document.addEventListener("keydown", (e) => {
+document.addEventListener('keydown', (e) => {
   switch (e.key) {
-    case "ArrowLeft":
+    case 'ArrowLeft':
       game.moveLeft();
       game.getScore();
       break;
-    case "ArrowRight":
+    case 'ArrowRight':
       game.moveRight();
       game.getScore();
       break;
-    case "ArrowUp":
+    case 'ArrowUp':
       game.moveUp();
       game.getScore();
       break;
-    case "ArrowDown":
+    case 'ArrowDown':
       game.moveDown();
       game.getScore();
       break;
